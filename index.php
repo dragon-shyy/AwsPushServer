63255a0408d90130e0b2e807f039594c5439a6d9
dragon-shyy <ylshyy@amazon.com>
2014-07-27 09:18:10 +0800

    2nd

--- index.php
+++ index.php
@@ -132,7 +132,7 @@ if(empty($errorMessage))
   <form action="index.php" method="post">
     <p>
       Notification Message:<br>
-      <input type="text" name="formMessage" maxlength="50" value="<?=$varMessage;?>" />
+      <input type="text" name="formMessage" maxlength="50" value=“<?php =$varMessage;?>" />
     </p>
     <input type="submit" name="formSubmit" value="Submit" />
   </form>
